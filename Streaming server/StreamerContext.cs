e87dcbd63ae18efa33d09cbcfae423c1de3dbf26
Jaka Kordez <jaka.kordez@gmail.com>
2023-11-03 14:04:44 +0100

    Source database WIP

---
+++ Streaming server/StreamerContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Streamer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Streaming_server
+{
+    public class StreamerContext:DbContext
+    {
+        public StreamerContext(DbContextOptions<StreamerContext> options) : base(options)
+        {
+        }
+
+        public DbSet<ISource> Sources { get; set; }
+    }
+}
